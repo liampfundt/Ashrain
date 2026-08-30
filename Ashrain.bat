5b8dcd7608cbc88febed5dc2ec5d1cdf46781b1c
FranzeDCG <franzedcg@gmail.com>
2026-08-30 22:56:35 +0200

    modified:   Ashrain.bat

--- Ashrain.bat
+++ Ashrain.bat
@@ -79,7 +79,7 @@ IF %ERRORLEVEL% == 4 GOTO :Ashrain.exit
 IF %ERRORLEVEL% == 5 GOTO :Ashrain.info
 IF %ERRORLEVEL% == 6 GOTO :Ashrain.licensing
 IF %ERRORLEVEL% == 7 GOTO :Ashrain.shell
-GOTO :Setup.error
+GOTO :Error.setup
 
 :: Run the guided setup
 :Setup.guided
@@ -91,18 +91,18 @@ ECHO └────────────────────┘ Choose a
 CALL :Function.oslist
 SET /P "Choice.os=└─────── Please enter the version number: "
 
-IF "%Choice.os%"=="10.7"  SET "Choice.os.selected=[92mMac OS X Lion 10.7[0m"
-IF "%Choice.os%"=="10.8"  SET "Choice.os.selected=[92mOS X Mountain Lion 10.8[0m"
-IF "%Choice.os%"=="10.10" SET "Choice.os.selected=[92mOS X Yosemite 10.10[0m"
-IF "%Choice.os%"=="10.11" SET "Choice.os.selected=[92mOS X El Capitan 10.11[0m"
-IF "%Choice.os%"=="10.12" SET "Choice.os.selected=[38;5;46mmacOS Sierra 10.12[0m"
-IF "%Choice.os%"=="10.13" SET "Choice.os.selected=[38;5;46mmacOS High Sierra 10.13[0m"
-IF "%Choice.os%"=="10.14" SET "Choice.os.selected=[38;5;46mmacOS Mojave 10.14[0m"
-IF "%Choice.os%"=="10.15" SET "Choice.os.selected=[32mmacOS Catalina 10.15[0m"
-IF "%Choice.os%"=="11"    SET "Choice.os.selected=[33mmacOS Big Sur 11[0m"
-IF "%Choice.os%"=="12"    SET "Choice.os.selected=[38;5;208mmacOS Monterey 12[0m"
-IF "%Choice.os%"=="13"    SET "Choice.os.selected=[31mmacOS Ventura 13[0m"
-IF "%Choice.os%"=="14"    SET "Choice.os.selected=[91mmacOS Sonoma 14[0m"
+IF "%Choice.os%"=="10.7"  SET "Choice.os.selected=[92mMac OS X Lion 10.7[0m" &&           SET "Setup.os.download=10,7"
+IF "%Choice.os%"=="10.8"  SET "Choice.os.selected=[92mOS X Mountain Lion 10.8[0m" &&      SET "Setup.os.download=10,8"
+IF "%Choice.os%"=="10.10" SET "Choice.os.selected=[92mOS X Yosemite 10.10[0m" &&          SET "Setup.os.download=10,10"
+IF "%Choice.os%"=="10.11" SET "Choice.os.selected=[92mOS X El Capitan 10.11[0m" &&        SET "Setup.os.download=10,11"
+IF "%Choice.os%"=="10.12" SET "Choice.os.selected=[38;5;46mmacOS Sierra 10.12[0m" &&      SET "Setup.os.download=10,12"
+IF "%Choice.os%"=="10.13" SET "Choice.os.selected=[38;5;46mmacOS High Sierra 10.13[0m" && SET "Setup.os.download=10,13"
+IF "%Choice.os%"=="10.14" SET "Choice.os.selected=[38;5;46mmacOS Mojave 10.14[0m" &&      SET "Setup.os.download=10,14"
+IF "%Choice.os%"=="10.15" SET "Choice.os.selected=[32mmacOS Catalina 10.15[0m" &&         SET "Setup.os.download=10,15"
+IF "%Choice.os%"=="11"    SET "Choice.os.selected=[33mmacOS Big Sur 11[0m" &&             SET "Setup.os.download=11"
+IF "%Choice.os%"=="12"    SET "Choice.os.selected=[38;5;208mmacOS Monterey 12[0m" &&      SET "Setup.os.download=12"
+IF "%Choice.os%"=="13"    SET "Choice.os.selected=[31mmacOS Ventura 13[0m" &&             SET "Setup.os.download=13"
+IF "%Choice.os%"=="14"    SET "Choice.os.selected=[91mmacOS Sonoma 14[0m" &&              SET "Setup.os.download=14"
 CHOICE /N /C YN /M "└─────── Are you sure to download %Choice.os.selected% (Y/n): "
 
 IF %ERRORLEVEL% == 1 GOTO :Setup.guided.download
@@ -111,22 +111,36 @@ IF %ERRORLEVEL% == 2 GOTO :Setup.guided
 :: Download the image file
 :Setup.guided.download
 CALL :Function.clearscreen
+CALL :Function.banner
+ECHO ┌────────────────────┐  Please wait while Ashrain is downloading the required image file.                                              
+ECHO │    Guided Setup    │  The rest of the process is automated by Ashrain.                  
+ECHO └────────────────────┘  Note: This might take a while depending on your internet and storage speed.                                   
+ECHO ┌──────────────────────────────────────────────┐                                                                  
+ECHO │            Ashrain Process Log               │                                                                  
+ECHO └──────────────────────────────────────────────┘                                                                  
+:: Check internet connection
+ECHO [[38;5;46mEXECUTE[0m] PING archive.org -n 5
+PING archive.org -n 5 >NUL
+IF %ERRORLEVEL% NEQ 0 CALL :Error.internetconnection
+ECHO.
+ECHO [[94mINFO[0m] Internet connection test successful
+ECHO.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+:: Download the wanted OS using aria2c
+ECHO [[38;5;46mEXECUTE[0m] Downloading required image...
+ECHO.
+IF "%Setup.os.download%" == "10,7" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%MacOSXLion10,7.iso" "%Download.Lion10,7%"   
+IF "%Setup.os.download%" == "10,8" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%OSXMountainLion10,8.iso" "%Download.MountainLion10,8%"
+IF "%Setup.os.download%" == "10,10" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%OSXYoseMite10,10.iso" "%Download.Yosemite10,10%"
+IF "%Setup.os.download%" == "10,11" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%OSXElCapitan10,11.iso" "%Download.ElCapitan10,11%"
+IF "%Setup.os.download%" == "10,12" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSSierra10,12.iso" "%Download.Sierra10,12%"
+IF "%Setup.os.download%" == "10,13" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSHighSierra10,13.iso" "%Download.HighSierra10,13%"
+IF "%Setup.os.download%" == "10,14" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSMojave10,14.iso" "%Download.Mojave10,14%"
+IF "%Setup.os.download%" == "10,15" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSCatalina10,15.iso" "%Download.Catalina10,15%"
+IF "%Setup.os.download%" == "11" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSBigSur11.iso" "%Download.BigSur11%"
+IF "%Setup.os.download%" == "12" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSMonterey12.iso" "%Download.Monterey12%"
+IF "%Setup.os.download%" == "13" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSVentura13.iso" "%Download.Ventura13%"
+IF "%Setup.os.download%" == "14" "%Path.aria2c%" -x 16 -s 16 --check-certificate=false -o "%Path.images%macOSSonoma14.iso" "%Download.Sonoma14%"
 
 
 
@@ -179,3 +193,5 @@ GOTO :EOF
 :Shell.function.Return
 CALL :Function.clearscreen
 GOTO :Ashrain.menu
+
+:: Error block (Put all errors in this section)
